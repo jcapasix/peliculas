6300befccf70b65fa08dc6c88a1215dbe99609eb
Jordan Capa <jcapasix@gmail.com>
2024-11-16 12:40:59 +0800

    The initial structure of the project was created

---
+++ Presentation/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Presentation
+//
+//  Created by Jordan Capa on 16/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
